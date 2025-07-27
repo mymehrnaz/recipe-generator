097c002d9f5d324488dfeb3db12ff72732ba2b45
mymehrnaz <165458618+mymehrnaz@users.noreply.github.com>
2025-07-27 09:59:25 +0330

    add the files

---
+++ src/main.js
@@ -0,0 +1,28 @@
+function displayRecipe(respose){
+    new Typewriter("#recipe", {
+  strings: respose.data.answer,
+  autoStart: true,
+  delay: 1,
+  cursor: "",
+});
+
+}
+
+function generateRecipe(event){
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector("#user-instructions");
+    let apiKey = "t5cfo950cb740d63be0666e8faf23d69";
+    let context = "You are a chef expert and love to give great and easy recipes. Your mission is to generate a short and easy recipe in basic HTML and seperate each line of the recipe with a <br/>. Make sure to follow the user instructions. Do not include a title to the recipe. Sign the recipe with 'SheCodes AI' inside a <strong> element at the end of the recipe and not at the beginning.Mke sure to provide a clear and precise answer";
+    let prompt = `User instructions: Generate a Food recipe about ${instructionsInput.value}`;
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    let recipeElement = document.querySelector("#recipe");
+    recipeElement.classList.remove("hidden");
+    recipeElement.innerHTML = `<div class="generating">⏳ </div> Generating a Food Recipe about ${instructionsInput.value}`;
+
+    axiox.get(apiURL).then(displayRecipe);
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
